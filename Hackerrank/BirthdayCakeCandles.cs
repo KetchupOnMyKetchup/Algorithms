5d6e879d2ea767dd389b7826f2206ebea615c20a
Crystal Tenn <catenn@gmail.com>
2019-02-13 22:31:36 -0500

    bday cake candles

---
+++ Hackerrank/BirthdayCakeCandles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hackerrank
+{
+    // https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    public class BirthdayCakeCandles
+    {
+        static int birthdayCakeCandles_problem(int[] ar)
+        {
+            // find highest number
+            int maxHeight = ar.Max();
+
+            // find how many 
+            int count = 0;
+
+            foreach (int candleHeight in ar)
+            {
+                if (maxHeight == candleHeight) count++;
+            }
+
+            return count;
+        }
+
+        // Insert into console
+        // 4
+        // 3 1 2 3
+
+        //static void Main(string[] args)
+        //{
+        //    int arCount = Convert.ToInt32(Console.ReadLine());
+
+        //    int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+        //    int result = birthdayCakeCandles(ar);
+        //}
+    }
+}
