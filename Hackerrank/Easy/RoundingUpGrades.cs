bbc5714ec3c2bc963fd65970593f89563d2af560
Crystal Tenn <catenn@gmail.com>
2019-03-04 19:44:12 -0500

    rounding up algo

---
+++ Hackerrank/Easy/RoundingUpGrades.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hackerrank.Easy
+{
+    public class RoundingUpGrades
+    {
+        // https://www.hackerrank.com/challenges/grading/problem
+        static int[] gradingStudents(int[] grades)
+        {
+            int[] roundedGrades = new int[grades.Count()];
+
+            int i = 0;
+
+            foreach (int grade in grades)
+            {
+                roundedGrades[i] = grade;
+
+                if (grade > 37)
+                {
+                    int remainder = grade % 5;
+
+                    if (remainder > 2) roundedGrades[i] = grade + (5 - remainder);
+                }
+
+                i++;
+            }
+
+            return roundedGrades;
+        }
+        //static void Main(string[] args)
+        //{
+        //    int n = Convert.ToInt32(Console.ReadLine());
+
+        //    int[] grades = new int[n];
+
+        //    for (int gradesItr = 0; gradesItr < n; gradesItr++)
+        //    {
+        //        int gradesItem = Convert.ToInt32(Console.ReadLine());
+        //        grades[gradesItr] = gradesItem;
+        //    }
+
+        //    int[] result = gradingStudents(grades);
+        //}
+    }
+}
--- Hackerrank/Program.cs
+++ Hackerrank/Program.cs
@@ -22,6 +22,7 @@ namespace Hackerrank
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
             
