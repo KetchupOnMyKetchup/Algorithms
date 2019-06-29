5866d6d6248cdd1375677ba4644803aee52854da
Crystal Tenn <crtenn@microsoft.com>
2019-06-29 15:33:56 -0400

    add roman to int

--- Algorithms/Algorithms/Cram/Ad-hoc/MaxProfit.cs
+++ Algorithms/Algorithms/Cram/Ad-hoc/MaxProfit.cs
@@ -26,9 +26,9 @@ namespace Algorithms.Algorithms.Cram.Ad_hoc
             return maxProfit;
         }
 
-        static void Main()
-        {
-            SolveMaxProfit(new[] { 7, 1, 5, 3, 6, 4 });
-        }
+        //static void Main()
+        //{
+        //    SolveMaxProfit(new[] { 7, 1, 5, 3, 6, 4 });
+        //}
     }
 }
--- Algorithms/Algorithms/Cram/Ad-hoc/RomanNumerals.cs
+++ Algorithms/Algorithms/Cram/Ad-hoc/RomanNumerals.cs
@@ -6,5 +6,57 @@ namespace Algorithms.Algorithms.Cram.Ad_hoc
 {
     class RomanNumerals
     {
+        static Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        public static int RomanToInt(string s) // IV = 4, VII = 7
+        {
+            SeedDictionary();
+            int result = 0;
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (CheckSpecialCase(s[i], s[i + 1]))
+                {
+                    stack.Push(dict.GetValueOrDefault(s[i + 1]) - dict.GetValueOrDefault(s[i]));
+                    i++;
+                }
+                else
+                {
+                    stack.Push(dict.GetValueOrDefault(s[i]));
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+
+            return result;
+        }
+
+        private static bool CheckSpecialCase(char x, char y)
+        {
+            if (dict.GetValueOrDefault(x) < dict.GetValueOrDefault(y)) return true;
+            return false;
+        }
+
+        private static void SeedDictionary()
+        {
+            dict.Add('I', 1);
+            dict.Add('V', 5);
+            dict.Add('X', 10);
+            dict.Add('L', 50);
+            dict.Add('C', 100);
+            dict.Add('D', 500);
+            dict.Add('M', 1000);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInt("IV"));
+            Console.ReadLine();
+        }
     }
 }
