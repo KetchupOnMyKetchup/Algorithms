1134c95360e6774cb8f5c707aaca38c2694eea28
Crystal Tenn <crtenn@microsoft.com>
2019-10-17 20:04:26 -0400

    complete chapter 1 problems from CTCI

--- Algorithms/Algorithms/CTCI/Ch1/08ZeroMatrix.cs
+++ Algorithms/Algorithms/CTCI/Ch1/08ZeroMatrix.cs
@@ -1,29 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Algorithms.Algorithms.CTCI.Ch1
 {
-    class _08ZeroMatrix
+    static class _08ZeroMatrix
     {
         private static object ZeroMatrix(int[][] matrix)
         {
-            throw new NotImplementedException();
-        }
+            int[][] finalMatrix = new int[matrix.Length][];
 
-        public static void Main()
-        {
-            int[][] matrix = new int[3][]
+            //TODO Finish later
+            //for (int i = 0; i < finalMatrix.Length; i++)
+            //{
+            //    finalMatrix[i] = Enumerable. new int[matrix.Length];
+            //}
+
+            for (int i = 0; i < matrix.Length; i++)
             {
-                new int[] { 1, 2 , 3},
-                new int[] { 4, 5, 6 },
-                new int[] { 7, 8, 9 }
-            };
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        NullifyRow(finalMatrix, i);
+                        NullifyColumn(finalMatrix, j);
+                    }
+                    else
+                    {
+                        if (finalMatrix[i][j] != 0) finalMatrix[i][j] = matrix[i][j];
+                    }
+                }
+            }
 
-            var result = ZeroMatrix(matrix);
-
-            Console.WriteLine(result);
-            Console.ReadLine();
+            return finalMatrix;
         }
+
+        static void NullifyRow(int[][] matrix, int row)
+        {
+            for (int i = 0; i < matrix.Length; i++) matrix[row][i] = 0;
+        }
+
+        static void NullifyColumn(int[][] matrix, int col)
+        {
+            for (int i = 0; i < matrix.Length; i++) matrix[i][col] = 0;
+        }
+
+        //public static void Main()
+        //{
+        //    int[][] matrix = new int[3][]
+        //    {
+        //        new int[] { 1, 0 ,3 },
+        //        new int[] { 4, 5, 6 },
+        //        new int[] { 7, 8, 9 }
+        //    };
+
+        //    var result = ZeroMatrix(matrix);
+
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
     }
 }
---
+++ Algorithms/Algorithms/CTCI/Ch1/09StringRotation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.CTCI.Ch1
+{
+    class _09StringRotation
+    {
+        private static bool IsSubstring(string s1, string s2)
+        {
+            if (s1.Length == s2.Length && s1.Length != 0)
+            {
+                StringBuilder sb = new StringBuilder(s1);
+                var s1s1 = sb.Append(s1).ToString();
+                return s1s1.Contains(s2);
+            }
+
+            return false;
+        }
+
+        //public static void Main()
+        //{
+        //    string s1 = "waterbottle";
+        //    string s2 = "terbottlewa";
+
+        //    var result = IsSubstring(s1, s2);
+
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
+    }
+}
