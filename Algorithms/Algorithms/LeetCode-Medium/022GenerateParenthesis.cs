1d5bc4ff56f81e49ad3f6e4293ee364046669c25
Crystal Tenn <crtenn@microsoft.com>
2019-11-22 23:13:23 -0500

    add generate parenthesis problem

---
+++ Algorithms/Algorithms/LeetCode-Medium/022GenerateParenthesis.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.LeetCode_Medium
+{
+    class _022GenerateParenthesis
+    {
+        //static void Main()
+        //{
+        //    int num = 2;
+
+        //    List<string> result = GenerateParenthesis(num);
+
+        //    foreach (var item in result)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+        private static List<string> GenerateParenthesis(int num)
+        {
+            List<string> list = new List<string>();
+            Backtrack(list, "", 0, 0, num);
+            return list;
+        }
+
+        public static void Backtrack(List<string> list, string str, int open, int close, int max)
+        {
+            if (str.Length == max * 2)
+            {
+                list.Add(str);
+                return;
+            }
+
+            if (open < max)
+                Backtrack(list, str + "(", open + 1, close, max);
+            if (close < open)
+                Backtrack(list, str + ")", open, close + 1, max);
+        }
+    }
+}
