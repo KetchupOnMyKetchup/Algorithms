76e8b01a4b5901928dc0dcdb3e523505f73966f5
Crystal Tenn <crtenn@microsoft.com>
2019-11-22 22:52:17 -0500

    add problems

---
+++ Algorithms/Algorithms/Cram/Ad-hoc/Add2LL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.Cram.Ad_hoc
+{
+    class Add2LL
+    {
+        //static void Main(string[] args)
+        //{
+        //    Node l1 = new Node(1);
+        //    l1.Next = new Node(2);
+        //    l1.Next.Next = new Node(3);
+
+        //    Node l2 = new Node(2);
+        //    l2.Next = new Node(3);
+        //    l2.Next.Next = new Node(4);
+
+        //    Console.WriteLine(MergeSumLL(l1, l2));
+        //    Console.ReadLine();
+        //}
+
+        class Node
+        {
+            public Node(int value) { Value = value; }
+            public Node Next;
+            public int Value;
+        }
+
+        private static Node MergeSumLL(Node l1, Node l2)
+        {
+            // O(n) time required, no space requirements
+
+            // create new LL and iterate through both and add
+
+            // to save space, iterate through both and just add value to first LL
+
+            Node dummy = new Node(0);
+            dummy.Next = l1;
+
+            while (l1 != null && l2 != null)
+            {
+                l1.Value += l2.Value;
+
+                l1 = l1.Next;
+                l2 = l2.Next;
+            }
+
+            return dummy.Next;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Cram/Ad-hoc/Merge2Arrays.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.Cram.Ad_hoc
+{
+    class Merge2Arrays
+    {
+        //static void Main(string[] args)
+        //{
+        //    int[] arr1 = { 1, 2, 7, 0, 0, 0 };
+        //    int[] arr2 = { 3, 5, 8 };
+
+        //    var results = MergeArrs(arr1, 3, arr2, 3);
+
+        //    foreach (var item in results)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+        private static int[] MergeArrs(int[] arr1, int length1, int[] arr2, int length2)
+        {
+            length1 -= 1;
+            length2 -= 1;
+
+            for (int i = arr1.Length - 1; i >= 0; i--)
+            {
+                if (length2 < 0 || arr1[length1] > arr2[length2])
+                {
+                    arr1[i] = arr1[length1];
+                    length1--;
+                }
+                else
+                {
+                    arr1[i] = arr2[length2];
+                    length2--;
+                }
+            }
+
+            return arr1;
+        }
+    }
+}
