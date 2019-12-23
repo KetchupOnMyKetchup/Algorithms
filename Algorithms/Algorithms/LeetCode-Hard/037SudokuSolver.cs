9759546b62dd419dc5737fb64a571e5e02b8d414
Crystal Tenn <crtenn@microsoft.com>
2019-12-22 22:50:41 -0500

    recursive practice

--- Algorithms/Algorithms/LeetCode-Hard/037SudokuSolver.cs
+++ Algorithms/Algorithms/LeetCode-Hard/037SudokuSolver.cs
@@ -6,29 +6,31 @@ namespace Algorithms.Algorithms.LeetCode_Hard
 {
     class _037SudokuSolver
     {
-        public static void Main()
-        {
-            char[][] board = new char[9][]
-            {
-                new char[] { '5','3','.','.','7','.','.','.','.' },
-                new char[] { '6','.','.','1','9','5','.','.','.' },
-                new char[] { '.','9','8','.','.','.','.','6','.' },
-                new char[] { '8','.','.','.','6','.','.','.','3' },
-                new char[] { '4','.','.','8','.','3','.','.','1' },
-                new char[] { '7','.','.','.','2','.','.','.','6' },
-                new char[] { '.','6','.','.','.','.','2','8','.' },
-                new char[] { '.','.','.','4','1','9','.','.','5' },
-                new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
-            };
+        //public static void Main()
+        //{
+        //    char[][] board = new char[9][]
+        //    {
+        //        new char[] { '5','3','.','.','7','.','.','.','.' },
+        //        new char[] { '6','.','.','1','9','5','.','.','.' },
+        //        new char[] { '.','9','8','.','.','.','.','6','.' },
+        //        new char[] { '8','.','.','.','6','.','.','.','3' },
+        //        new char[] { '4','.','.','8','.','3','.','.','1' },
+        //        new char[] { '7','.','.','.','2','.','.','.','6' },
+        //        new char[] { '.','6','.','.','.','.','2','8','.' },
+        //        new char[] { '.','.','.','4','1','9','.','.','5' },
+        //        new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+        //    };
 
-            SolveSudoku(board);
-            Console.ReadLine();
-        }
+        //    SolveSudoku(board);
+        //    Console.ReadLine();
+        //}
 
         public static void SolveSudoku(char[][] board)
         {
             CanSolveSudokuFromCell(0, 0, board);
         }
+
+        private static char EMPTY_ENTRY = '.';
 
         public static bool CanSolveSudokuFromCell(int row, int col, char[][] board)
         {
@@ -38,13 +40,12 @@ namespace Algorithms.Algorithms.LeetCode_Hard
             {
                 col = 0;
                 row++;
+
+                if (row == board.Length) return true;
             }
 
-            // finished with entire board
-            if (row == board.Length) return true;
-
             // no need to solve a cell if it already has a value
-            if (board[col][row] != '.') return CanSolveSudokuFromCell(row, col + 1, board);
+            if (board[row][col] != EMPTY_ENTRY) return CanSolveSudokuFromCell(row, col + 1, board);
 
             // Solve
             for (int val = 1; val <= 9; val++)
@@ -56,7 +57,7 @@ namespace Algorithms.Algorithms.LeetCode_Hard
                 {
                     board[row][col] = c;
                     if (CanSolveSudokuFromCell(row, col + 1, board)) return true;
-                    board[row][col] = '.';
+                    board[row][col] = EMPTY_ENTRY;
                 }
             }
 
@@ -111,91 +112,6 @@ namespace Algorithms.Algorithms.LeetCode_Hard
 
         // Java B2B solution
 
-        private static char EMPTY_ENTRY = '.';
-
-        public static void SolveSudoku(char[][] board)
-        {
-            canSolveSudokuFromCell(0, 0, board);
-        }
-
-        private static bool canSolveSudokuFromCell(int row, int col, char[][] board)
-        {
-            if (col == board[row].Length)
-            {
-                col = 0;
-                row++;
-
-                if (row == board.Length)
-                {
-                    return true;
-                }
-            }
-
-            // Skip entries already filled out. They already have a value in them.
-            if (board[row][col] != EMPTY_ENTRY)
-            {
-                return canSolveSudokuFromCell(row, col + 1, board);
-            }
-
-            for (int value = 1; value <= board.Length; value++)
-            {
-                char charToPlace = (char)(value + '0');
-
-                if (canPlaceValue(board, row, col, charToPlace))
-                {
-                    board[row][col] = charToPlace;
-                    if (canSolveSudokuFromCell(row, col + 1, board))
-                    {
-                        return true;
-                    }
-                    board[row][col] = EMPTY_ENTRY;
-                }
-            }
-
-            return false;
-        }
-
-        private static bool canPlaceValue(char[][] board, int row, int col, char charToPlace)
-        {
-            // Check column of the placement
-            foreach (char[] placementRow in board)
-            {
-                if (charToPlace == placementRow[col])
-                {
-                    return false;
-                }
-            }
-
-            // Check row of the placement
-            for (int i = 0; i < board[row].Length; i++)
-            {
-                if (charToPlace == board[row][i])
-                {
-                    return false;
-                }
-            }
-
-            // Check region constraints - get the size of the sub-box
-            int regionSize = (int)Math.Sqrt(board.Length);
-
-            int verticalBoxIndex = row / regionSize;
-            int horizontalBoxIndex = col / regionSize;
-
-            int topLeftOfSubBoxRow = regionSize * verticalBoxIndex;
-            int topLeftOfSubBoxCol = regionSize * horizontalBoxIndex;
-
-            for (int i = 0; i < regionSize; i++)
-            {
-                for (int j = 0; j < regionSize; j++)
-                {
-                    if (charToPlace == board[topLeftOfSubBoxRow + i][topLeftOfSubBoxCol + j])
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
+        
     }
 }
---
+++ Algorithms/Algorithms/LeetCode-Medium/017PhoneNumber.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.LeetCode_Medium
+{
+    class _017PhoneNumber
+    {
+        //public static void Main()
+        //{
+        //    string digits = "23";
+
+        //    var result = LetterCombinations(digits);
+
+        //    foreach (var item in result)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+        static Dictionary<char, string> dict = new Dictionary<char, string>() 
+        {
+            { '2', "abc" },
+            { '3', "def" },
+            { '4', "ghi" },
+            { '5', "jkl" },
+            { '6', "mno" },
+            { '7', "pqrs" },
+            { '8', "tuv" },
+            { '9', "wxyz" },
+        };
+
+
+        public static IList<string> LetterCombinations(string digits)
+        {
+            List<string> finalResult = new List<string>();
+
+            if (digits.Length == 0) return finalResult;
+
+            BackTrack(finalResult, digits, 0, new StringBuilder(""));
+
+            return finalResult;
+        }
+
+        public static void BackTrack(List<string> finalResult, string digits, int currentIndex, StringBuilder partialMnemonic)
+        {
+            // Constraints: we could run out of numbers
+            // if no letters left, return
+            if (currentIndex == digits.Length)
+            {
+                finalResult.Add(partialMnemonic.ToString());
+                return;
+            }
+
+            // Choice: one letter at a time per #
+            foreach (var letter in dict[digits[currentIndex]])
+            {
+                partialMnemonic.Append(letter);
+
+                BackTrack(finalResult, digits, currentIndex + 1, partialMnemonic);
+
+                partialMnemonic.Remove(partialMnemonic.Length - 1, 1);
+            }
+
+            // Goal: Get full list of letters possible
+        }
+    }
+}
