89b52c02809b18122dc90e3b8825e95fa519d15e
Crystal Tenn <crtenn@microsoft.com>
2019-12-24 01:00:25 -0500

    restore IP addresses

---
+++ Algorithms/Algorithms/LeetCode-Hard/051N-Queens.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.LeetCode_Hard
+{
+    class _051N_Queens
+    {
+        //public static void Main()
+        //{
+        //    int n = 3;
+
+        //    var result = SolveNQueens(n);
+
+        //    foreach (var item in result)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+        //public static IList<IList<string>> SolveNQueens(int n)
+        //{
+
+        //}
+    }
+}
---
+++ Algorithms/Algorithms/LeetCode-Medium/093RestoreIPAddresses.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.LeetCode_Medium
+{
+    class _093RestoreIPAddresses
+    {
+        public static void Main()
+        {
+            //string s = "0000";
+            string s = "010010";
+            // 0.0.0.0 is valid
+            // 255.255.255.255 is valid
+
+            // 255.023.255.255 is NOT valid
+            // 255.23.255.255 is valid
+
+            var result = RestoreIpAddresses(s);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static IList<string> RestoreIpAddresses(string s)
+        {
+            List<string> finalResult = new List<string>();
+
+            string[] segments = new string[4];
+            SplitIp(finalResult, segments, s, 0, 0);
+
+            return finalResult;
+        }
+
+        private static void SplitIp(List<string> finalResult, string[] segments, string s, int pointer, int segmentNum)
+        {
+            if (segmentNum == 4)
+            {
+                if (pointer == s.Length) finalResult.Add(segments[0] + "." + segments[1] + "." + segments[2] + "." + segments[3]);
+                return;
+            }
+
+            for (int i = 1; i <= 3; i++)
+            {
+                if (pointer + i > s.Length) return;
+
+                string temp = s.Substring(pointer, i);
+                if (temp[0] == '0' && temp.Length > 1) continue;
+
+                int.TryParse(temp.ToString(), out int res);
+
+                if (res <= 255)
+                {
+                    segments[segmentNum] = temp;
+                    SplitIp(finalResult, segments, s, pointer + temp.Length, segmentNum + 1);
+                }
+
+            }
+        }
+    }
+}
