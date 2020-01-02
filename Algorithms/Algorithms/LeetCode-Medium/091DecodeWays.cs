6545a4ad8cbfa27ca2a059fad3a0f648a9167f86
Crystal Tenn <crtenn@microsoft.com>
2020-01-01 19:20:12 -0500

    # decodings progress

--- Algorithms/Algorithms/LeetCode-Medium/091DecodeWays.cs
+++ Algorithms/Algorithms/LeetCode-Medium/091DecodeWays.cs
@@ -16,9 +16,28 @@ namespace Algorithms.Algorithms.LeetCode_Medium
 
         public static int NumDecodings(string s)
         {
+            StringBuilder sb = new StringBuilder();
 
+            BackTrack(new List<string>(), s, 0, sb);
 
             return 0;
+        }
+
+        private static void BackTrack(List<string> list, string s, int index, StringBuilder sb)
+        {
+
+
+
+            for (int i = index; i < 2; i++)
+            {
+                if (int.Parse(sb.Append(s[i]).ToString()) > 26)
+                {
+                    sb.Remove(sb.Length - 1, 1);
+                    return;
+                }
+
+                BackTrack(list, s, index + i, sb);
+            }
         }
     }
 }
