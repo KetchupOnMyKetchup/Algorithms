0263e4a36abb4dec79e8a5023eb1cb3dd0d7dc0f
Crystal Tenn <crtenn@microsoft.com>
2020-01-02 20:27:25 -0500

    decode

--- Algorithms/Algorithms/LeetCode-Medium/091DecodeWays.cs
+++ Algorithms/Algorithms/LeetCode-Medium/091DecodeWays.cs
@@ -8,36 +8,86 @@ namespace Algorithms.Algorithms.LeetCode_Medium
     {
         public static void Main()
         {
-            int result = NumDecodings("12");
+            var result = NumDecodings("12");
 
             Console.WriteLine(result);
             Console.ReadLine();
         }
 
-        public static int NumDecodings(string s)
+        /// <summary>
+        /// Dynamic Programming method for getting count only
+        /// </summary>
+        //public static int NumDecodings(string s)
+        //{
+        //    int n = s.Length;
+        //    if (n == 0) return 0;
+
+        //    int[] memo = new int[n + 1];
+
+        //    memo[n] = 1;
+        //    memo[n - 1] = s[n - 1] != '0' ? 1 : 0;
+
+        //    for (int i = n - 2; i >= 0; i--)
+        //    {
+        //        if (s[i] == '0') continue;
+        //        else
+        //        {
+        //            if (int.Parse(s.Substring(i, 2)) <= 26)
+        //            {
+        //                memo[i] = memo[i + 1] + memo[i + 2];
+        //            }
+        //            else
+        //            {
+        //                memo[i] = memo[i + 1];
+        //            }
+        //        }
+        //    }
+
+        //    return memo[0];
+        //}
+
+        /// <summary>
+        /// Recursive method for returning full list of combinations if that scenario was asked
+        /// </summary>
+        public static List<string> NumDecodings(string s)
         {
-            StringBuilder sb = new StringBuilder();
+            List<string> list = new List<string>();
 
-            BackTrack(new List<string>(), s, 0, sb);
+            char[] charArr = new char[27];
+            charArr[0] = (char)('A' - 1);
 
-            return 0;
+            for (int i = 1; i < charArr.Length; i++)
+            {
+                charArr[i] = (char)(charArr[i - 1] + 1);
+            }
+
+            Backtrack(s, 0, new StringBuilder(), list, charArr);
+
+            return list;
         }
 
-        private static void BackTrack(List<string> list, string s, int index, StringBuilder sb)
+        private static void Backtrack(string s, int startIndex, StringBuilder partialString, List<string> list, char[] charArr)
         {
-
-
-
-            for (int i = index; i < 2; i++)
+            if (startIndex == s.Length)
             {
-                if (int.Parse(sb.Append(s[i]).ToString()) > 26)
-                {
-                    sb.Remove(sb.Length - 1, 1);
-                    return;
-                }
-
-                BackTrack(list, s, index + i, sb);
+                string temp = partialString.Remove(partialString.Length - 1, 1).ToString();
+                list.Add(temp);
+                return;
             }
+
+            for (int i = 1; i <= 2; i++)
+            {
+                // check if <= 26
+                string temp = s.Substring(startIndex, i);
+                int num = int.Parse(temp);
+
+                if (num <= 26) partialString.Append(charArr[num] + " ");
+                else i = 0;
+
+                Backtrack(s, startIndex + i, partialString, list, charArr);
+
+            }
+
         }
     }
 }
--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -1,22 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpTutorial
 {
     class Program
     {
+        // Scenario: Create a Console app that takes in 2 numbers and allows you to add/sub/mult/divide them and prints the result
         static void Main(string[] args)
         {
 
-            string input = Console.ReadLine();
-            int intTemp = Convert.ToInt32(Console.ReadLine());
-
-            Add(int.Parse(input), 2);
-            Console.WriteLine("Hello World!");
-        }
-
-        public static int Add(int num1, int num2)
-        {
-            return 1;
         }
     }
 }
