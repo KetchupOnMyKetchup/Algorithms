6dcdd1095dc0cef53f6615cf7b08fe9741919114
Crystal Tenn <crtenn@microsoft.com>
2020-01-04 00:27:05 -0500

    Greedy algorithm - scheduling

--- Algorithms/Algorithms/LeetCode-Medium/322CoinChange.cs
+++ Algorithms/Algorithms/LeetCode-Medium/322CoinChange.cs
@@ -6,24 +6,45 @@ namespace Algorithms.Algorithms.LeetCode_Medium
 {
     class _322CoinChange
     {
-        public static void Main()
-        {
-            int[] coins = new int[] { 1, 2, 5 };
-            var result = CoinChange(coins, 11);
+        //public static void Main()
+        //{
+        //    int[] coins = new int[] { 1, 2, 5 };
+        //    var result = CoinChange(coins, 11);
 
-            Console.WriteLine(result);
-            Console.ReadLine();
-        }
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
 
         public static int CoinChange(int[] coins, int amount)
         {
-            if ()
-            {
+            if (amount < 1) return 0;
 
+            return Helper(coins, amount, new int[amount]);
+        }
+
+        private static int Helper(int[] coins, int remainder, int[] dp)
+        {
+            if (remainder < 0) 
+                return -1; // invalid solution
+            if (remainder == 0) 
+                return 0; // valid solution
+            if (dp[remainder - 1] != 0)
+                return dp[remainder - 1]; // already completed, re-use
+
+            int min = int.MaxValue;
+
+            foreach (var coin in coins)
+            {
+                int res = Helper(coins, remainder - coin, dp);
+
+                if (res >= 0 && res < min) // only save valid solutions as min
+                {
+                    min = res + 1; // +1 to add 1 coin that we're testing out
+                }
             }
 
-
-            return 0;
+            dp[remainder - 1] = (min == int.MaxValue) ? -1 : min;
+            return dp[remainder - 1];
         }
     }
 }
---
+++ Algorithms/Algorithms/LeetCode-Medium/435NonOverlappingIntervals.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.LeetCode_Medium
+{
+    /// <summary>
+    /// Greedy Algorithm -- not an algo, its a technique
+    /// https://en.wikipedia.org/wiki/Interval_scheduling#Interval_Scheduling_Maximization
+    /// 1. Select the interval, x, with the earliest finishing time.
+    //  2. Remove x, and all intervals intersecting x, from the set of candidate intervals.
+    //  3. Repeat until the set of candidate intervals is empty.
+    /// </summary>
+    class _435NonOverlappingIntervals
+    {
+        public static void Main()
+        {
+            int[][] intervals = new int[4][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 2, 3 },
+                new int[] { 3, 4 },
+                new int[] { 1, 3 }
+            };
+            var result = EraseOverlapIntervals(intervals);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static int EraseOverlapIntervals(int[][] intervals)
+        {
+            if (intervals.Length == 0) return 0;
+            
+            Array.Sort(intervals, new MyComparator());
+            int end = intervals[0][1];
+            int count = 1;
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (intervals[i][0] >= end)
+                {
+                    end = intervals[i][1];
+                    count++;
+                }
+            }
+
+            return intervals.Length - count;
+        }
+    }
+    class MyComparator : Comparer<int[]>
+    {
+        public override int Compare(int[] x, int[] y)
+        {
+            return x[1] - y[1];
+        }
+    }
+}
