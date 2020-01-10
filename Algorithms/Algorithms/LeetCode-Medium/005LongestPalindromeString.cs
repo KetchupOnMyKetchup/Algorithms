35a568d56672e99a21ec2f9b79c0908387be3cac
Crystal Tenn <crtenn@microsoft.com>
2020-01-10 00:48:29 -0500

    palindrome and word ladder

---
+++ Algorithms/Algorithms/LeetCode-Medium/005LongestPalindromeString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Algorithms.LeetCode_Medium
+{
+    class _005LongestPalindromeString
+    {
+        //public static void Main()
+        //{
+        //    var result = LongestPalindrome("asaaztacocatt");
+
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
+
+        static int lo, maxLen;
+
+        public static string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return "";
+            if (s.Length < 2) return s;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                ExpandAroundCenter(s, i, i); // odd
+                ExpandAroundCenter(s, i, i + 1); // even
+            }
+
+            return s.Substring(lo, maxLen - 1); // -1 to makeup for length of starting char
+        }
+
+        private static void ExpandAroundCenter(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+
+            if (maxLen < right - left)
+            {
+                lo = left + 1; // add 1 back from last iteration on line 37
+                maxLen = right - left;
+            }
+        }
+    }
+}
--- Algorithms/Algorithms/LeetCode-Medium/127WordLadder.cs
+++ Algorithms/Algorithms/LeetCode-Medium/127WordLadder.cs
@@ -6,78 +6,118 @@ namespace Algorithms.Algorithms.LeetCode_Medium
 {
     class _127WordLadder
     {
-        public static void Main()
-        {
-            IList<string> wordList = new List<string>() {"hot", "dot", "dog", "lot", "log", "cog"};
+        //public static void Main()
+        //{
+        //    IList<string> wordList = new List<string>() {"hot", "dot", "dog", "lot", "log", "cog"};
 
-            var result = LadderLength("hit", "cog", wordList);
+        //    var result = LadderLength("hit", "cog", wordList);
 
-            Console.WriteLine(result);
-            Console.ReadLine();
-        }
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
 
-        class Word
-        {
-            public Word(string word, int distanceFromStart)
-            {
-                Value = word;
-                DistanceFromStart = distanceFromStart;
-                LinkedWords = new List<Word>();
-            }
-
-            public string Value { get; set; }
-            public List<Word> LinkedWords { get; set; }
-            public int DistanceFromStart { get; set; }
-        }
-
+        // https://leetcode.com/problems/word-ladder/discuss/40704/Java-Solution-using-BFS-with-explanation
+        // does not pass all unit tests with java or C#, see other solution that is commented out or solution tab in leetcode
         public static int LadderLength(string beginWord, string endWord, IList<string> wordList)
         {
-            // Set start word
-            Word start = new Word(beginWord, 1);
-            Word curr = start;
+            HashSet<string> reached = new HashSet<string>();
 
-            // Loop over other words
-            return Recurse(endWord, wordList, curr, "");
-        }
+            reached.Add(beginWord);
+            wordList.Add(endWord);
+            int distance = 1;
 
-        private static int Recurse(string endWord, IList<string> wordList, Word curr, string prev)
-        {
-            // test if can get to end word
-            if (Is1SubstitutionAway(curr, endWord)) 
-                return curr.DistanceFromStart + 1;
-
-            foreach (var word in wordList)
+            while (!reached.Contains(endWord))
             {
-                if (Is1SubstitutionAway(curr, word)) // See if 1 substitution away
+                HashSet<string> toAdd = new HashSet<string>();
+                foreach (var item in reached)
                 {
-                    if (prev == word) continue; // prevent circular loop **TODO make more hardy than prev, prevent cyclic like last problem
-                    curr.LinkedWords.Add(new Word(word, ++curr.DistanceFromStart));
+                    for (int i = 0; i < item.Length; i++)
+                    {
+                        char[] chars = item.ToCharArray();
+
+                        for (char ch = 'a'; ch <= 'z'; ch++)
+                        {
+                            chars[i] = ch;
+                            string word = new string(chars);
+
+                            if (wordList.Contains(word))
+                            {
+                                toAdd.Add(word);
+                                wordList.Remove(word);
+                            }
+
+                        }
+                    }
                 }
-
+                distance++;
+                if (toAdd.Count == 0) return 0;
+                reached = toAdd;
             }
-
-            foreach (var nextWord in curr.LinkedWords)
-            {
-                Recurse(endWord, wordList, nextWord, curr.Value);
-            }
-
-            return 0;
+            return distance;
         }
 
-        private static bool Is1SubstitutionAway(Word curr, string word)
-        {
-            int count = 0;
+        //class Word
+        //{
+        //    public Word(string word, int distanceFromStart)
+        //    {
+        //        Value = word;
+        //        DistanceFromStart = distanceFromStart;
+        //        LinkedWords = new List<Word>();
+        //    }
 
-            for (int i = 0; i < curr.Value.Length; i++)
-            {
-                for (int j = 0; j < word.Length; j++)
-                {
-                    if (curr.Value[i] == word[j]) count++;
-                }
-            }
+        //    public string Value { get; set; }
+        //    public List<Word> LinkedWords { get; set; }
+        //    public int DistanceFromStart { get; set; }
+        //}
 
-            if (count == curr.Value.Length - 1) return true;
-            return false;
-        }
+        //public static int LadderLength(string beginWord, string endWord, IList<string> wordList)
+        //{
+        //    // Set start word
+        //    Word start = new Word(beginWord, 1);
+        //    Word curr = start;
+
+        //    // Loop over other words
+        //    return Recurse(endWord, wordList, curr, "");
+        //}
+
+        //private static int Recurse(string endWord, IList<string> wordList, Word curr, string prev)
+        //{
+        //    // test if can get to end word
+        //    if (Is1SubstitutionAway(curr, endWord)) 
+        //        return curr.DistanceFromStart + 1;
+
+        //    foreach (var word in wordList)
+        //    {
+        //        if (Is1SubstitutionAway(curr, word)) // See if 1 substitution away
+        //        {
+        //            if (prev == word) continue; // prevent circular loop **TODO make more hardy than prev, prevent cyclic like last problem
+        //            curr.LinkedWords.Add(new Word(word, ++curr.DistanceFromStart));
+        //        }
+
+        //    }
+
+        //    foreach (var nextWord in curr.LinkedWords)
+        //    {
+        //        Recurse(endWord, wordList, nextWord, curr.Value);
+        //    }
+
+        //    return 0;
+        //}
+
+        //private static bool Is1SubstitutionAway(Word curr, string word)
+        //{
+        //    int count = 0;
+
+        //    for (int i = 0; i < curr.Value.Length; i++)
+        //    {
+        //        for (int j = 0; j < word.Length; j++)
+        //        {
+        //            if (curr.Value[i] == word[j]) count++;
+        //        }
+        //    }
+
+        //    if (count == curr.Value.Length - 1) return true;
+        //    return false;
+        //}
     }
 }
