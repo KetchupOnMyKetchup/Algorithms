34ba66a214a192ebd9dcaaf4ffd5289f8fbad93e
Crystal Tenn <crtenn@microsoft.com>
2020-02-11 00:11:47 -0500

    add LL cycle problem

--- Algorithms/Algorithms/LeetCode-Easy/141LLCycle.cs
+++ Algorithms/Algorithms/LeetCode-Easy/141LLCycle.cs
@@ -19,6 +19,10 @@ namespace Algorithms.Algorithms.LeetCode_Easy
         static void Main()
         {
             ListNode node = new ListNode(3);
+            node.next = new ListNode(2);
+            node.next.next = new ListNode(0);
+            node.next.next.next = new ListNode(4);
+            node.next.next.next.next = node.next;
 
             var result = HasCycle(node);
             Console.WriteLine(result);
@@ -26,8 +30,20 @@ namespace Algorithms.Algorithms.LeetCode_Easy
 
         public static bool HasCycle(ListNode head)
         {
+            if (head == null || head.next == null) return false;
 
-            return false;
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (slow != fast)
+            {
+                if (fast == null || fast.next == null) return false;
+
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return true;
         }
     }
 }
