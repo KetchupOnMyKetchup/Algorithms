9e6dbb677d390a18d2c52db8b18f122b11af61db
Crystal Tenn <crtenn@microsoft.com>
2020-02-19 19:10:46 -0500

    add another way to do group anagrams

--- Algorithms/Algorithms/LeetCode-Medium/GroupAnagrams.cs
+++ Algorithms/Algorithms/LeetCode-Medium/GroupAnagrams.cs
@@ -16,24 +16,62 @@ namespace Algorithms.Algorithms.LeetCode_Medium
         public static IList<IList<string>> GroupAnagrams(string[] strs)
         {
             Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            int[] count = new int[26];
 
             for (int i = 0; i < strs.Length; i++)
             {
-                char[] arr = strs[i].ToCharArray();
-                Array.Sort(arr);
-                string sorted = new string(arr);
+                Array.Fill(count, 0);
 
-                if (dict.ContainsKey(sorted))
+                char[] arr = strs[i].ToCharArray();
+
+                foreach (var c in arr)
                 {
-                    dict[sorted].Add(strs[i]);
+                    count[c - 'a']++;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < 26; j++)
+                {
+                    sb.Append('#');
+                    sb.Append(count[j]);
+                }
+
+                string key = sb.ToString();
+
+                if (dict.ContainsKey(key))
+                {
+                    dict[key].Add(strs[i]);
                 }
                 else
                 {
-                    dict.Add(sorted, new List<string>() { strs[i] });
+                    dict.Add(key, new List<string>() { strs[i] });
                 }
             }
 
             return new List<IList<string>>(dict.Values);
         }
+
+        //public static IList<IList<string>> GroupAnagrams(string[] strs)
+        //{
+        //    Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+        //    for (int i = 0; i < strs.Length; i++)
+        //    {
+        //        char[] arr = strs[i].ToCharArray();
+        //        Array.Sort(arr);
+        //        string sorted = new string(arr);
+
+        //        if (dict.ContainsKey(sorted))
+        //        {
+        //            dict[sorted].Add(strs[i]);
+        //        }
+        //        else
+        //        {
+        //            dict.Add(sorted, new List<string>() { strs[i] });
+        //        }
+        //    }
+
+        //    return new List<IList<string>>(dict.Values);
+        //}
     }
 }
